b91ce7188cb57bee9584869dad4fc623af9d05fd
Makhuba Pride Vhampfunaho <134763779+Pridemakhuba@users.noreply.github.com>
2026-09-07 14:03:08 +0200

    Delete ONT-GRP-03-10.Server directory

--- ONT-GRP-03-10.Server/Services/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PRS.Backend.Services;
-
-public interface IEmailService
-{
-    Task SendAsync(string toEmail, string toName, string subject, string htmlBody);
-    Task SendEvaluationAssignedAsync(string evaluatorEmail, string evaluatorName, string proposalTitle);
-    Task SendProposalStatusUpdateAsync(string studentEmail, string studentName, string proposalTitle, string status);
-    Task SendSupervisorSignoffRequestAsync(string supervisorEmail, string supervisorName, string studentName, string proposalTitle);
-}
